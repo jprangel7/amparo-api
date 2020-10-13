1eaac2da64b83f767c67cb84a32efcb44d4ea11c
Joao Pedro Rangel <jpmrangel22@gmail.com>
2020-10-13 05:16:00 -0300

    Get activities by date, status and cpf added

--- src/routes/activity.routes.ts
+++ src/routes/activity.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { getRepository } from "typeorm";
-import { isBefore, parseISO } from "date-fns";
 
 import Activity from "../models/Activity";
+import Patient from "../models/Patient";
 
 import CreateActivityService from "../services/CreateActivityService";
 import CreateActivityUsingPatientNameService from "../services/CreateActivityUsingPatientNameService";
@@ -16,6 +16,51 @@ activityRouter.get("/", async (request, response) => {
   const allActivities = await activityRepository.find();
 
   return response.status(200).json(allActivities);
+});
+
+activityRouter.get("/findByDate", async (request, response) => {
+  const { date } = request.body;
+
+  const activityRepository = getRepository(Activity);
+
+  const filteredActivities = await activityRepository.find({
+    where: { expiration_date: new Date(date) },
+  });
+
+  return response.status(200).json(filteredActivities);
+});
+
+activityRouter.get("/findByStatus", async (request, response) => {
+  const { status } = request.body;
+
+  const activityRepository = getRepository(Activity);
+
+  const filteredActivities = await activityRepository.find({
+    where: { status: status },
+  });
+
+  return response.status(200).json(filteredActivities);
+});
+
+activityRouter.get("/findByCPF", async (request, response) => {
+  const { cpf } = request.body;
+
+  const activityRepository = getRepository(Activity);
+  const patientRepository = getRepository(Patient);
+
+  const patient = await patientRepository.findOne({
+    where: { cpf: cpf },
+  });
+
+  let filteredActivities: Activity[] = [];
+
+  if (patient) {
+    filteredActivities = await activityRepository.find({
+      where: { patient_id: patient.id },
+    });
+  }
+
+  return response.status(200).json(filteredActivities);
 });
 
 activityRouter.post("/", async (request, response) => {
@@ -38,12 +83,7 @@ activityRouter.post("/newActivity", async (request, response) => {
 
   const createActivity = new CreateActivityUsingPatientNameService();
 
-  await createActivity.execute(
-    patient,
-    expiration_date,
-    status,
-    activity
-  );
+  await createActivity.execute(patient, expiration_date, status, activity);
 
   const responseData = {
     patient_id: {
